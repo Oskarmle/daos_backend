62af950862164b0c58bb899a99f7d4753aa27627
Oskar Eriksen <omle.eriksen@gmail.com>
2024-12-19 18:05:07 +0100

    updated main .ts file

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.MONGODB_URI],
+    origin: [process.env.MONGODB_URI || '*'],
     methods: 'GET,POST,PUT,PATCH,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
