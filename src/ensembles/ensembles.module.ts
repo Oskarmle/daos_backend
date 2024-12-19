682e036f2b6043e9ae8558f0947349ba2375a0dd
Oskar Eriksen <omle.eriksen@gmail.com>
2024-12-19 17:49:08 +0100

    updated naming

--- src/ensembles/ensembles.module.ts
+++ src/ensembles/ensembles.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { EnsemblesService } from './ensembles.service';
 import { EnsemblesController } from './ensembles.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Ensemble, EnsembleSchema } from './schemas/Ensemble.schema';
+import { Ensemble, EnsembleSchema } from './schemas/ensemble.schema';
 // import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from 'src/auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
